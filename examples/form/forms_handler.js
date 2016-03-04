80d4ccf8055ac37bd7d88bf16c4a9a0496fb65fd
yevgeny-zakharov <yevgeny-zakharov@seotime.ru>
2016-03-04 14:39:36 +0300

    Комментарии к примеру формы

--- examples/form/forms_handler.js
+++ examples/form/forms_handler.js
@@ -12,51 +12,65 @@ function blink(form, selector) {
     jQuery(form).find(selector).fadeIn(200);
 }
 
-$(function() {
-    var selectorButton = '.sendLead';
-    var selectorForm = '.leadForm';
-    var selectorStatusBlock = '#status';
-    var selectorPhone = '#phone';
+$(function () {
+    // Селекторы ключевых элементов формы с которыми будем взаимодействовать
+    var selectorButton = '.sendLead'; // Кнопка
+    var selectorForm = '.leadForm'; // Тело формы
+    var selectorStatusBlock = '#statusForm'; // Пустой блок для статуса (успех или ошибка)
+    var selectorPhone = '#phone'; // Телефон
 
-   $(selectorButton).click(function() {
+    // Когда клик по кнопке "Отправить"
+    $(selectorButton).click(function () {
 
-       var sendButton = $(this);
-       var form = $(sendButton).parents(selectorForm);
-       var phone = $(form).find(selectorPhone);
+        // Инициируем объекты отталкиваясь от нажатой кнопки "Отправить",
+        // чтобы этот обработчик можно было повесить на несколько форм
+        var sendButton = $(this); // Объект кнопки "Отправить"
+        var form = $(sendButton).parents(selectorForm); // Объект текущей формы, отталкиваясь от нажатой кнопки
+        var phone = $(form).find(selectorPhone); // Объект инпута "Ваш номер", отталкиваясь от текущей формы
 
-       var phone_value = $(phone).val();
-       phone_value = phone_value.replace(/[ \-()+]/g, "");
-       var phone_length = phone_value.length;
+        // Переменная с номером телефона
+        var phone_value = $(phone).val();
+        // Обрезаем все лишние символы которые ввёл пользователь чтобы определить корректную длину телефона
+        phone_value = phone_value.replace(/[ \-()+]/g, "");
+        var phone_length = phone_value.length; // Длина телефона
 
-       if (phone_length == 10 || phone_length == 11 || phone_length == 12) {
+        // Если длина телефона 10, 11 или 12
+        if (phone_length == 10 || phone_length == 11 || phone_length == 12) {
 
-           jQuery(phone).removeAttr('style');
+            // Сериализуем форму для отправки
+            var serialize_form = jQuery(form).serialize();
 
-           var serialize_form = jQuery(form).serialize();
+            jQuery.ajax({
+                type: "POST",
+                url: "forms_handler.php",
+                data: serialize_form,
+                context: document.body
+            }).done(function (success) {
 
-           jQuery.ajax({
-               type: "POST",
-               url: "forms_handler.php",
-               data: serialize_form,
-               context: document.body
-           }).done(function (success) {
-               if (success == 'error') {
+                // Если в ответе fors_handler.php мы получили error
+                if (success == 'error') {
+                    $(form).find(selectorStatusBlock).html('<hr><div class="alert alert-danger" role="alert" style="text-align: center;">К сожалению, произошла ошибка. <br>Пожалуйста, попробуйте ещё раз</div>');
 
-                   $(form).find('#statusForm').html('<hr><div class="alert alert-danger" role="alert" style="text-align: center;">К сожалению, произошла ошибка. <br>Пожалуйста, попробуйте ещё раз</div>');
+                } else { // Если error'а не было
 
-               } else {
-                   $(form).find('#statusForm').html('<hr><div class="alert alert-success" role="alert" style="text-align: center;">Данные отправлены. В ближайшее время мы свяжемся с Вами</div>');
+                    $(form).find(selectorStatusBlock).html('<hr><div class="alert alert-success" role="alert" style="text-align: center;">Данные отправлены. В ближайшее время мы свяжемся с Вами</div>');
 
-                   jQuery(sendButton).attr('disabled', 'disabled').text('Отправлено');
-                   jQuery(form).find('input,textarea').attr('disabled', 'disabled');
+                    // Отключаем форму
+                    jQuery(sendButton).attr('disabled', 'disabled').text('Отправлено');
+                    jQuery(form).find('input,textarea').attr('disabled', 'disabled');
 
-                   console.log(success);
-                   $('#post').text(success);
-               }
-           });
-       } else {
-           $(phone).css('border', '1px solid red');
-           blink(form, selectorPhone);
-       }
-   });
+                    console.log(success);
+                    $('#post').text(success);
+                }
+
+
+                // Удаляем "border: '1px solid red;" у инпута "Ваш телефон"
+                jQuery(phone).removeAttr('style');
+            });
+        } else { // Если длинна телефона не 10,11 или 12
+            $(phone).css('border', '1px solid red');
+            // Мигание
+            blink(form, selectorPhone);
+        }
+    });
 });
--- examples/form/forms_handler.php
+++ examples/form/forms_handler.php
@@ -1,17 +1,20 @@
 <?php
 include_once('../../app/functions.php');
-print_r($_POST);
 $domain = 'siteminibase.seo-time.ru';
 $formKey = 'lead';
+
+print_r($_POST);
+
+// Если в $_POST массиве есть элемент с ключом lead и он не пустой
 if (isset($_POST[$formKey]) AND count($_POST[$formKey])) {
-
     $subject = 'EazyBreezy Form - Срочно перезвонить!';
-    $message = arrayToMessage($_POST[$formKey]);
 
+    // Формируем текст из массива
+    $message = arrayToMessage($_POST[$formKey]);
     $headers = 'From: noreply@' . $domain . "\r\n" .
         'Reply-To: noreply@' . $domain . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
-
+    // Рассылаем всем получателям из массива
     foreach ($recipients as $to) {
         mail($to, $subject, $message, $headers);
     }
